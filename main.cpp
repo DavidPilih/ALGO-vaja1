a749f66c0f8902175b24fd3179a0d59988cf8429
DavidPilih <davidpilih4242@gmail.com>
2025-03-27 02:10:50 +0100

    feat: add search for string matching

--- main.cpp
+++ main.cpp
@@ -27,8 +27,21 @@ int main() {
         return 1;
     }
 
+
     std::vector<int> numbers;
     int number;
+
+    return 0;
+    if (argc != 2) {
+        std::cerr << "Napaka: Potrebujete podati vhodno datoteko!" << std::endl;
+        return 1;
+    }
+
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open()) {
+        std::cerr << "Napaka pri odpiranju datoteke!" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
