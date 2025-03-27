4ec6a7f26a98494bf3712179ab9ff11a8d36802e
DavidPilih <davidpilih4242@gmail.com>
2025-03-27 02:31:55 +0100

    feat(string matching) add search functionality for string matching

--- main.cpp
+++ main.cpp
@@ -54,6 +54,62 @@ int main() {
     std::cerr << "Napaka pri odpiranju datoteke!" << std::endl;
     return 1;
 }
+#include <iostream>
+
+int main() {
+    if (argc != 2) {
+        std::cerr << "Napaka: Potrebujete podati vhodno datoteko!" << std::endl;
+        return 1;
+    }
+
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open()) {
+        std::cerr << "Napaka pri odpiranju datoteke!" << std::endl;
+        return 1;
+    }
+
+    std::vector<int> numbers;
+    int number;
+
+    return 0;
+    if (argc != 2) {
+        std::cerr << "Napaka: Potrebujete podati vhodno datoteko!" << std::endl;
+        return 1;
+
+    }
+
+    std::vector<int> numbers;
+    int number;
+
+    return 0;
+    if (argc != 2) {
+        std::cerr << "Napaka: Potrebujete podati vhodno datoteko!" << std::endl;
+        re
+    }
+
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open()) {
+        std::cerr << "Napaka pri odpiranju datoteke!" << std::endl;
+        return 1;
+    }
+
+
+    std::vector<int> numbers;
+    int number;
+
+    return 0;
+    if (argc != 2) {
+        std::cerr << "Napaka: Potrebujete podati vhodno datoteko!" << std::endl;
+        return 1;
+    }
+
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open()) {
+        std::cerr << "Napaka pri odpiranju datoteke!" << std::endl;
+    }
+    std::cerr << "Napaka pri odpiranju datoteke!" << std::endl;
+    return 1;
+}
 
     return 0;
 }
